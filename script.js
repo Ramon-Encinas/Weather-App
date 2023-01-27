845b9220882e3207282154d1678920a7b41647f5
Ramon <ramon.enra@gmail.com>
2023-01-26 17:16:00 -0800

    Information now correctly displays on website

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ let weather = {
     
 
 // Functionally the same as fetchWeathers, this one is using async/await   
-async function fetchWeather(city) {
+async function displayWeather(city) {
 
     // Get json file from API 
     let response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
@@ -28,23 +28,29 @@ async function fetchWeather(city) {
     // Extract the desired data from json file and log it into console
     const getData = (cityData) => {
         const { name } = cityData;
-        const { icon, description} = cityData.weather[0]; 
         // Since the data is inside an array we need to select the 1st element in order to access its date
+        const { icon, description} = cityData.weather[0]; 
         const { temp, humidity } = cityData.main; 
         const { speed } = cityData.wind
         console.log(name, temp, icon, description, humidity, speed);
-        }
-    
-
+        
+        // Display the desired data into the divs 
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";       
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".description").innerText = description.charAt(0).toUpperCase() + description.slice(1);
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = 'Wind Speed: ' + speed + " km/h";
+        };
+        
     // Call function
     getData(cityData);
-
-}
-
+        
+    }
+    
 
 //Test it with different cities
-fetchWeather("Tijuana");
-fetchWeather("Chicago");
+displayWeather("Berlin");
 
 
 
